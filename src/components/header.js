0eacb733343d73c489eda0dd89dd769383a9023d
Elena <elenamargayova@gmail.com>
2018-06-05 17:43:49 +0300

    simple search is finished

--- src/components/header.js
+++ src/components/header.js
@@ -1,25 +1,11 @@
-import React, {Component} from 'react';
-import '../css/styles.css'
-class Header extends Component {
+import React from 'react';
+import classes from '../css/styles.css';
 
-state = {
-    keywords: ''
-  }
 
-  inputChangeHandler=(event) =>{
-    //console.log(event.target.value)
-    //const value = event.target.value ==='' ? 'active':'non-active';
-    this.setState({
-      keywords: event.target.value
-    });
-  }
-
-  render() {
-
-    return (<header className={this.state.active}>
-      <div className="logo">Logo</div>
-      <input type="text" onChange={this.inputChangeHandler}/>
+const Header = (props)=>{
+    return (<header>
+      <div className={classes.logo}>Logo</div>
+      <input type="text" onChange={props.keywords  }/>
     </header>)
-  }
 }
 export default Header;
--- src/components/news_list_item.js
+++ src/components/news_list_item.js
@@ -2,7 +2,7 @@ import React from 'react';
 import classes from '../css/styles.css';
 const NewsItem = ({item})=>{
   return (
-    <div className={classes.news Item}>
+    <div className={classes.newsItem}>
       <h3>{item.title}</h3>
       <div>
         {item.feed}
